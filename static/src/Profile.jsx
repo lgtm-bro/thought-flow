6336c4a1fd842aa0f24271f731564f33c3e2969c
Bonnie Owens <arlolocos@gmail.com>
2022-12-19 23:06:21 -0500

    Profile skeleton

---
+++ static/src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Profile = (props) => {
+  return (
+    <div>
+      <h2>Edit Profile</h2>
+      <form>
+
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
